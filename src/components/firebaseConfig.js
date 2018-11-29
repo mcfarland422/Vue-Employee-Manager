5e4973b4a881829d6cd6372a042cbd17341def9a
Michael McFarland <michael.mcfarland@luckie.com>
2018-11-29 16:03:00 -0500

    Added firebase config files

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDSgs3iLKkoWEkxqYLKOYGlPbyyF9hrFL4",
+    authDomain: "viefs-prod-44ef4.firebaseapp.com",
+    databaseURL: "https://viefs-prod-44ef4.firebaseio.com",
+    projectId: "viefs-prod-44ef4",
+    storageBucket: "viefs-prod-44ef4.appspot.com",
+    messagingSenderId: "430641456611"
+}
