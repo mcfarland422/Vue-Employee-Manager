7b983c5c012a62f3c46d873dc1b2f3814aeca907
Michael McFarland <michael.mcfarland@luckie.com>
2018-11-29 16:16:36 -0500

    Added Component files

---
+++ src/components/EditEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="edit-emloyee">
+        <h3>Edit Employee</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'edit-employee',
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="new-employee">
+        <h3>New Employee</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'new-emloyee',
+    data () {
+        return {
+            
+        }
+    }
+}
+</script>
